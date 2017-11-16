db7677bf8e571458bfdf69548ae4c9208f6a03a3
Skycoder42 <Skycoder42@users.noreply.github.com>
2017-11-16 21:10:54 +0100

    added qpmx formula
    (and fixed tabs)

---
+++ Formula/qpmx.rb
@@ -0,0 +1,49 @@
+class Qpmx < Formula
+	desc "A frontend for qpm, to provide source and build caching"
+	homepage "https://github.com/Skycoder42/qpmx"
+	version "1.3.0"
+	url "https://github.com/Skycoder42/qpmx/archive/#{version}.tar.gz"
+	sha256 "d153308bc224ffc294429d274561436b1d19bc19286b3dd91136e128cd66e6ca"
+	
+	resource "qcliparser" do
+		url "https://github.com/Skycoder42/QCliParser/archive/1.0.0.tar.gz"
+		sha256 "043c0366c318243a3a32cde30942331b14685737b5174eb67b4d8c7cbe53d399"
+	end
+	
+	depends_on "qt"
+	depends_on "qtjsonserializer"
+	depends_on :xcode => :build
+	
+	def install
+		resource("qcliparser").stage { (buildpath/"submodules/qcliparser").install Dir["*"] }
+		
+		open(".qmake.conf", 'a') { |f|
+			f.puts "CONFIG += no_installer"
+		}
+		Dir.mkdir "build"
+		Dir.chdir "build"
+		
+		ENV["QMAKEPATH"] = "#{ENV["QMAKEPATH"]}:#{HOMEBREW_PREFIX}/Cellar/qtjsonserializer/#{Formula["qtjsonserializer"].version}"
+		system "qmake", "-config", "release", ".."
+		system "make", "qmake_all"
+		system "make"
+		
+		# ENV.deparallelize
+		instdir = "#{buildpath}/install"
+		system "make", "INSTALL_ROOT=#{instdir}", "install"
+		prefix.install Dir["#{instdir}#{HOMEBREW_PREFIX}/Cellar/qt/#{Formula["qt"].version}/*"]
+		
+		# adjust qt.conf
+		open(prefix/"bin/qt.conf", 'w') { |f|
+			f.puts "[Paths]"
+			f.puts "Prefix=#{HOMEBREW_PREFIX}/Cellar/qt/#{Formula["qt"].version}"
+			f.puts "Plugins=#{prefix}/plugins"
+		}
+	end
+	
+	test do
+		providers=`#{prefix}/bin/qpmx list providers --short`
+		prov=providers.strip.split(/ /)
+		return (prov.include? "git") && (prov.include? "qpm")
+	end
+end
--- Formula/qtjsonserializer.rb
+++ Formula/qtjsonserializer.rb
@@ -1,71 +1,71 @@
 class Qtjsonserializer < Formula
-  version "3.1.0"
-  desc "A library to perform generic seralization and deserialization of QObjects"
-  homepage "https://skycoder42.github.io/QtJsonSerializer/"
-  url "https://github.com/Skycoder42/QtJsonSerializer/archive/#{version}.tar.gz"
-  sha256 "d28780fb04f66bcadb1a6498d373d12a51bb2ce5d813d9e89629a0b9330936f5"
-
-  keg_only "Qt itself is keg only which implies the same for Qt modules"
-
-  option "with-docs", "Build documentation"
-
-  depends_on "qt"
-  depends_on :xcode => :build
-  depends_on "python3" => [:build, "with-docs"]
-  depends_on "doxygen" => [:build, "with-docs"]
-
-  def file_replace(file, base, suffix)
-    text = File.read(file)
-    replace = text.gsub(base, "#{base}/../../../qtjsonserializer/#{version}/#{suffix}")
-    File.open(file, "w") { |f| f << replace }
-  end
-
-  def install
-    Dir.mkdir ".git"
-    Dir.mkdir "build"
-    Dir.chdir "build"
-    system "qmake", "-config", "release", ".."
-    system "make", "qmake_all"
-    system "make"
-
-    if build.with? "docs"
-      system "make", "doxygen"
-    end
-
-    # ENV.deparallelize
-    instdir = "#{buildpath}/install"
-    system "make", "INSTALL_ROOT=#{instdir}", "install"
-    prefix.install Dir["#{instdir}#{HOMEBREW_PREFIX}/Cellar/qt/#{Formula["qt"].version}/*"]
-
-    # overwrite pri include
-    file_replace "#{prefix}/mkspecs/modules/qt_lib_jsonserializer.pri", "QT_MODULE_LIB_BASE", "lib"
-    file_replace "#{prefix}/mkspecs/modules/qt_lib_jsonserializer.pri", "QT_MODULE_BIN_BASE", "bin"
-    file_replace "#{prefix}/mkspecs/modules/qt_lib_jsonserializer_private.pri", "QT_MODULE_LIB_BASE", "lib"
-
-    #create bash src
-    File.open("#{prefix}/bashrc.sh", "w") { |file| file << "export QMAKEPATH=$QMAKEPATH:#{prefix}" }
-  end
-
-  test do
-    (testpath/"test.pro").write <<~EOS
-      CONFIG -= app_bundle
-      QT += jsonserializer
-      SOURCES += main.cpp
-    EOS
-
-    (testpath/"main.cpp").write <<~EOS
-      #include <QtCore>
-      #include <QtJsonSerializer>
-      int main() {
-        QJsonSerializer s;
-        qDebug() << s.serialize<int>(42);
-        return 0;
-      }
-    EOS
-
-    ENV["QMAKEPATH"] = "#{ENV["QMAKEPATH"]}:#{prefix}"
-    system "#{Formula["qt"].bin}/qmake", "test.pro"
-    system "make"
-    system "./test"
-  end
+	version "3.1.0"
+	desc "A library to perform generic seralization and deserialization of QObjects"
+	homepage "https://skycoder42.github.io/QtJsonSerializer/"
+	url "https://github.com/Skycoder42/QtJsonSerializer/archive/#{version}.tar.gz"
+	sha256 "d28780fb04f66bcadb1a6498d373d12a51bb2ce5d813d9e89629a0b9330936f5"
+	
+	keg_only "Qt itself is keg only which implies the same for Qt modules"
+	
+	option "with-docs", "Build documentation"
+	
+	depends_on "qt"
+	depends_on :xcode => :build
+	depends_on "python3" => [:build, "with-docs"]
+	depends_on "doxygen" => [:build, "with-docs"]
+	
+	def file_replace(file, base, suffix)
+		text = File.read(file)
+		replace = text.gsub(base, "#{base}/../../../qtjsonserializer/#{version}/#{suffix}")
+		File.open(file, "w") { |f| f << replace }
+	end
+	
+	def install
+		Dir.mkdir ".git"
+		Dir.mkdir "build"
+		Dir.chdir "build"
+		system "qmake", "-config", "release", ".."
+		system "make", "qmake_all"
+		system "make"
+		
+		if build.with? "docs"
+			system "make", "doxygen"
+		end
+		
+		# ENV.deparallelize
+		instdir = "#{buildpath}/install"
+		system "make", "INSTALL_ROOT=#{instdir}", "install"
+		prefix.install Dir["#{instdir}#{HOMEBREW_PREFIX}/Cellar/qt/#{Formula["qt"].version}/*"]
+		
+		# overwrite pri include
+		file_replace "#{prefix}/mkspecs/modules/qt_lib_jsonserializer.pri", "QT_MODULE_LIB_BASE", "lib"
+		file_replace "#{prefix}/mkspecs/modules/qt_lib_jsonserializer.pri", "QT_MODULE_BIN_BASE", "bin"
+		file_replace "#{prefix}/mkspecs/modules/qt_lib_jsonserializer_private.pri", "QT_MODULE_LIB_BASE", "lib"
+		
+		#create bash src
+		File.open("#{prefix}/bashrc.sh", "w") { |file| file << "export QMAKEPATH=$QMAKEPATH:#{prefix}" }
+	end
+	
+	test do
+		(testpath/"test.pro").write <<~EOS
+		CONFIG -= app_bundle
+		QT += jsonserializer
+		SOURCES += main.cpp
+		EOS
+		
+		(testpath/"main.cpp").write <<~EOS
+		#include <QtCore>
+		#include <QtJsonSerializer>
+		int main() {
+			QJsonSerializer s;
+		qDebug() << s.serialize<int>(42);
+		return 0;
+		}
+		EOS
+		
+		ENV["QMAKEPATH"] = "#{ENV["QMAKEPATH"]}:#{prefix}"
+		system "#{Formula["qt"].bin}/qmake", "test.pro"
+		system "make"
+		system "./test"
+	end
 end
