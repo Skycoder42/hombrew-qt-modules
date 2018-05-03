be407a45afa441adf7718f395f3a11e0bc9cbbd7
Skycoder42 <Skycoder42@users.noreply.github.com>
2018-05-03 21:51:27 +0200

    update qpmx formula

--- Formula/qpmx.rb
+++ Formula/qpmx.rb
@@ -1,20 +1,21 @@
 class Qpmx < Formula
 	desc "A frontend for qpm, to provide source and build caching"
 	homepage "https://github.com/Skycoder42/qpmx"
-	version "1.4.2"
+	version "1.5.0"
 	revision 1
 	url "https://github.com/Skycoder42/qpmx/archive/#{version}.tar.gz"
-	sha256 "8dfd17d8c9e48cdb6a48afc21bd2efa65bc0bb01c141fb145fd8d54ca85a472f"
+	sha256 "efa92bce06bbc813ccd56b6b16d3b4d79af988e6e8c28304ccdf635069aa050d"
 	
 	resource "qcliparser" do
-		url "https://github.com/Skycoder42/QCliParser/archive/1.0.0.tar.gz"
-		sha256 "043c0366c318243a3a32cde30942331b14685737b5174eb67b4d8c7cbe53d399"
+		url "https://github.com/Skycoder42/QCliParser/archive/1.0.1.tar.gz"
+		sha256 "713b20ccd4fe89ea663da142e1c4c2550b6e86f425b94552b638f97ff8785ae2"
 	end
 	
 	depends_on "qt"
 	depends_on "qtjsonserializer"
 	depends_on :xcode => :build
 	depends_on "qpm" => :recommended
+	depends_on "qbs" => :optional
 	
 	def install
 		resource("qcliparser").stage { (buildpath/"submodules/qcliparser").install Dir["*"] }
