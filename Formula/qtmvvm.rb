2a0a29eb8e869c29705fb1053a0ae205116508b7
Skycoder42 <Skycoder42@users.noreply.github.com>
2018-08-14 22:59:23 +0200

    ... to 1.1.2

--- Formula/qtmvvm.rb
+++ Formula/qtmvvm.rb
@@ -1,10 +1,10 @@
 class Qtmvvm < Formula
-	version "1.1.1"
+	version "1.1.2"
 	revision 1
 	desc "A mvvm oriented library for Qt, to create Projects for Widgets and Quick in parallel"
 	homepage "https://github.com/Skycoder42/QtMvvm"
 	url "https://github.com/Skycoder42/QtMvvm/archive/#{version}.tar.gz"
-	sha256 "1a90dbbe472abd15cd5e66c0ca5f6990436ed3db48b303737142131a180ee359"
+	sha256 "aac865c97ba3dde7b4515feb165732e405807b040427256df42b99e2fc61b9dc"
 	
 	keg_only "Qt itself is keg only which implies the same for Qt modules"
 	
