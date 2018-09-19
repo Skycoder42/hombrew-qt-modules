191cbd30fa6ec66e63af9b71259dfd878754b5bf
Skycoder42 <Skycoder42@users.noreply.github.com>
2018-09-19 16:35:59 +0200

    fix checksum

--- Casks/syrem.rb
+++ Casks/syrem.rb
@@ -1,7 +1,7 @@
 cask 'syrem' do
   version '3.0.0'
   url "https://github.com/Skycoder42/Syrem/releases/download/#{version}/syrem-#{version}_clang_64.dmg"
-  sha256 '7b54529cd00332e423839cf768b732ac6c42e17de9325d0a093764180deeb611'
+  sha256 '8818373ae1c4eff1fee9fc76a6c5cb7b0d83eb558325fc5e81bb478fb600b7fe'
   appcast 'https://github.com/Skycoder42/Syrem/releases.atom'
   
   name 'Syrem'
