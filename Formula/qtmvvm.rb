9c398908f73516cfabbe5059de3edcd50a74ccad
Skycoder42 <Skycoder42@users.noreply.github.com>
2019-03-01 16:26:18 +0100

    update mvvm formula

--- Formula/qtmvvm.rb
+++ Formula/qtmvvm.rb
@@ -1,10 +1,12 @@
-class Qtmvvm < Formula
-	version "1.1.3"
+require_relative "../base/Qtformula"
+
+class Qtmvvm < Qtformula
+	version "1.1.5"
 	revision 1
 	desc "A mvvm oriented library for Qt, to create Projects for Widgets and Quick in parallel"
 	homepage "https://github.com/Skycoder42/QtMvvm"
 	url "https://github.com/Skycoder42/QtMvvm/archive/#{version}.tar.gz"
-	sha256 "a669404ee1687986c8aea5f3a5f93df4136bf6a00715658ba692a90244a5ecc1"
+	sha256 "7e95322375c098d4f3de5e49cd5003571a435304ff8504d4b266d7a9fd1dd3f2"
 	
 	keg_only "Qt itself is keg only which implies the same for Qt modules"
 	
@@ -13,65 +15,26 @@ class Qtmvvm < Formula
 	depends_on "qt"
 	depends_on "qtdatasync" => :recommended
 	depends_on :xcode => :build
-	depends_on "qpmx" => :build
-	depends_on "qpm" => :build
+	depends_on "qdep" => :build
 	depends_on "python3" => [:build, "with-docs"]
 	depends_on "doxygen" => [:build, "with-docs"]
 	depends_on "graphviz" => [:build, "with-docs"]
 	
-	def file_replace(file, base, suffix)
-		text = File.read(file)
-		replace = text.gsub(base, "#{base}/../../../qtmvvm/#{pkg_version}/#{suffix}")
-		File.open(file, "w") { |f| f << replace }
-	end
-	
-	def install		
-		Dir.mkdir ".git"
-		Dir.mkdir "build"
-		Dir.chdir "build"
-		
+	def install
+		prepare_qdep
 		if build.with? "qtdatasync"
-			ENV["QMAKEPATH"] = "#{ENV["QMAKEPATH"]}:#{HOMEBREW_PREFIX}/Cellar/qtjsonserializer/#{Formula["qtjsonserializer"].pkg_version}"
-			ENV["QMAKEPATH"] = "#{ENV["QMAKEPATH"]}:#{HOMEBREW_PREFIX}/Cellar/qtdatasync/#{Formula["qtdatasync"].pkg_version}"
+			add_modules "qtdatasync", "qtjsonserializer"
 		end
-		ENV["QPMX_CACHE_DIR"] = "#{ENV["HOME"]}/qpmx-cache"
-		system "mkdir", "-p", "#{ENV["QPMX_CACHE_DIR"]}"
-		system "qmake", "-config", "release", ".."
-		system "make", "qmake_all"
-		system "make"
-		system "make", "lrelease"
-		
-		if build.with? "docs"
-			system "make", "doxygen"
+		build_and_install_default
+		create_mod_pri prefix, "mvvmcore"
+		create_tool_pri prefix, "qsettingsgenerator"
+		create_mod_pri prefix, "mvvmwidgets"
+		create_mod_pri prefix, "mvvmquick"
+		if build.with? "qtdatasync"
+			create_mod_pri prefix, "mvvmdatasynccore"
+			create_mod_pri prefix, "mvvmdatasyncwidgets"
+			create_mod_pri prefix, "mvvmdatasyncquick"
 		end
-		
-		# ENV.deparallelize
-		instdir = "#{buildpath}/install"
-		system "make", "INSTALL_ROOT=#{instdir}", "install"
-		prefix.install Dir["#{instdir}#{HOMEBREW_PREFIX}/Cellar/qt/#{Formula["qt"].pkg_version}/*"]
-		
-		# overwrite pri include
-		file_replace "#{prefix}/mkspecs/modules/qt_lib_mvvmcore.pri", "QT_MODULE_LIB_BASE", "lib"
-		file_replace "#{prefix}/mkspecs/modules/qt_lib_mvvmcore.pri", "QT_MODULE_BIN_BASE", "bin"
-		file_replace "#{prefix}/mkspecs/modules/qt_lib_mvvmcore_private.pri", "QT_MODULE_LIB_BASE", "lib"
-		file_replace "#{prefix}/mkspecs/modules/qt_lib_mvvmwidgets.pri", "QT_MODULE_LIB_BASE", "lib"
-		file_replace "#{prefix}/mkspecs/modules/qt_lib_mvvmwidgets.pri", "QT_MODULE_BIN_BASE", "bin"
-		file_replace "#{prefix}/mkspecs/modules/qt_lib_mvvmwidgets_private.pri", "QT_MODULE_LIB_BASE", "lib"
-		file_replace "#{prefix}/mkspecs/modules/qt_lib_mvvmquick.pri", "QT_MODULE_LIB_BASE", "lib"
-		file_replace "#{prefix}/mkspecs/modules/qt_lib_mvvmquick.pri", "QT_MODULE_BIN_BASE", "bin"
-		file_replace "#{prefix}/mkspecs/modules/qt_lib_mvvmquick_private.pri", "QT_MODULE_LIB_BASE", "lib"
-		file_replace "#{prefix}/mkspecs/modules/qt_lib_mvvmdatasynccore.pri", "QT_MODULE_LIB_BASE", "lib"
-		file_replace "#{prefix}/mkspecs/modules/qt_lib_mvvmdatasynccore.pri", "QT_MODULE_BIN_BASE", "bin"
-		file_replace "#{prefix}/mkspecs/modules/qt_lib_mvvmdatasynccore_private.pri", "QT_MODULE_LIB_BASE", "lib"
-		file_replace "#{prefix}/mkspecs/modules/qt_lib_mvvmdatasyncwidgets.pri", "QT_MODULE_LIB_BASE", "lib"
-		file_replace "#{prefix}/mkspecs/modules/qt_lib_mvvmdatasyncwidgets.pri", "QT_MODULE_BIN_BASE", "bin"
-		file_replace "#{prefix}/mkspecs/modules/qt_lib_mvvmdatasyncwidgets_private.pri", "QT_MODULE_LIB_BASE", "lib"
-		file_replace "#{prefix}/mkspecs/modules/qt_lib_mvvmdatasyncquick.pri", "QT_MODULE_LIB_BASE", "lib"
-		file_replace "#{prefix}/mkspecs/modules/qt_lib_mvvmdatasyncquick.pri", "QT_MODULE_BIN_BASE", "bin"
-		file_replace "#{prefix}/mkspecs/modules/qt_lib_mvvmdatasyncquick_private.pri", "QT_MODULE_LIB_BASE", "lib"
-		
-		#create bash src
-		File.open("#{prefix}/bashrc.sh", "w") { |file| file << "export QMAKEPATH=$QMAKEPATH:#{prefix}" }
 	end
 	
 	test do
