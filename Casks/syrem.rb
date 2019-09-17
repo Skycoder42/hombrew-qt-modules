8dc9778f4388ce086f1eb7e206deef9852a25161
Skycoder42 <Skycoder42@users.noreply.github.com>
2019-09-17 14:58:41 +0200

    dummy update [skip ci]

--- Casks/syrem.rb
+++ Casks/syrem.rb
@@ -1,5 +1,6 @@
 cask 'syrem' do
   version '3.0.0'
+  revision 2
   url "https://github.com/Skycoder42/Syrem/releases/download/#{version}/syrem-#{version}_clang_64.dmg"
   sha256 '8818373ae1c4eff1fee9fc76a6c5cb7b0d83eb558325fc5e81bb478fb600b7fe'
   appcast 'https://github.com/Skycoder42/Syrem/releases.atom'
