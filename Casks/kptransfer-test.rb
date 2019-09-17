f72403a55fddca56ccc2c8b6328fb5b6dd64c502
Skycoder42 <Skycoder42@users.noreply.github.com>
2019-09-17 15:03:22 +0200

    ... [skip ci]

--- Casks/kptransfer-test.rb
+++ Casks/kptransfer-test.rb
@@ -1,5 +1,5 @@
 cask 'kptransfer-test' do
-  version '3.0.0'
+  version '3.0.1'
   url "https://github.com/Skycoder42/KeepassTransfer/releases/download/3.0.0/kptransfer-3.0.0_clang_64.dmg"
   sha256 '997d0654288b9e69b289fda1529ed35eb8df0b141b599c00520bf379ea15b214'
   appcast 'https://github.com/Skycoder42/KeepassTransfer/releases.atom'
