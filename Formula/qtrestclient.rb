48c23f5ae2d6fb487dd466b6b12c76ebe1352cab
Skycoder42 <Skycoder42@users.noreply.github.com>
2020-01-31 07:12:30 +0100

    ....

--- Formula/qtrestclient.rb
+++ Formula/qtrestclient.rb
@@ -15,7 +15,7 @@ class Qtrestclient < Qtformula
 	depends_on "qt"
 	depends_on "qtjsonserializer"
 	depends_on :xcode => :build
-	#depends_on "qdep" => :build
+	depends_on "qdep" => :build
 	depends_on "python3" => [:build, "with-docs"]
 	depends_on "doxygen" => [:build, "with-docs"]
 	depends_on "graphviz" => [:build, "with-docs"]
