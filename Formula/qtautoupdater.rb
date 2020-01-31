bcdaf507749b0c1e2b4ebe25bf11d621b758a644
Skycoder42 <Skycoder42@users.noreply.github.com>
2020-01-31 08:51:40 +0100

    ..

--- Formula/qtautoupdater.rb
+++ Formula/qtautoupdater.rb
@@ -47,6 +47,10 @@ class Qtautoupdater < Qtformula
 		ENV["QMAKEPATH"] = "#{ENV["QMAKEPATH"]}:#{prefix}"
 		system "#{Formula["qt"].bin}/qmake", "test.pro"
 		system "make"
+		
+		ENV["QT_PLUGIN_PATH"] = "#{ENV["QT_PLUGIN_PATH"]}:#{prefix}/plugins"
+		ENV["DYLD_LIBRARY_PATH"] = "#{ENV["DYLD_FRAMEWORK_PATH"]}:#{prefix}/lib"
+		ENV["DYLD_FRAMEWORK_PATH"] = "#{ENV["DYLD_FRAMEWORK_PATH"]}:#{prefix}/lib"
 		system "./test"
 	end
 end
